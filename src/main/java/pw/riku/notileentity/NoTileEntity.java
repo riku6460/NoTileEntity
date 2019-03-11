7cf9c466e85cac33d130831abfbc36ae8f9fad2f
riku6460 <17585784+riku6460@users.noreply.github.com>
2019-03-11 18:14:31 +0900

    とりあえず動くようになった

--- src/main/java/pw/riku/notileentity/NoTileEntity.java
+++ src/main/java/pw/riku/notileentity/NoTileEntity.java
@@ -1,7 +1,117 @@
 package pw.riku.notileentity;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.wrappers.BlockPosition;
+import com.comphenix.protocol.wrappers.nbt.NbtBase;
+import com.comphenix.protocol.wrappers.nbt.NbtCompound;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.*;
 
 public final class NoTileEntity extends JavaPlugin {
 
+    private Set<UUID> players = new HashSet<>();
+    private List<String> blocks;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        blocks = getConfig().getStringList("blocks");
+        ProtocolManager manager = ProtocolLibrary.getProtocolManager();
+        manager.addPacketListener(new PacketAdapter(this, PacketType.Play.Server.TILE_ENTITY_DATA) {
+            @Override
+            public void onPacketSending(PacketEvent event) {
+                if (players.contains(event.getPlayer().getUniqueId())) {
+                    BlockPosition position = event.getPacket().getBlockPositionModifier().read(0);
+                    Material material = position.toLocation(event.getPlayer().getWorld()).getBlock().getType();
+                    if (blocks.contains(material.name())) {
+                        event.setCancelled(true);
+                    }
+                }
+            }
+        });
+        manager.addPacketListener(new PacketAdapter(this, PacketType.Play.Server.MAP_CHUNK) {
+            @Override
+            public void onPacketSending(PacketEvent event) {
+                if (players.contains(event.getPlayer().getUniqueId())) {
+                    List<NbtBase<?>> list = event.getPacket().getListNbtModifier().read(0);
+                    Iterator<NbtBase<?>> iterator = list.iterator();
+                    while (iterator.hasNext()) {
+                        NbtBase<?> base = iterator.next();
+                        if (base instanceof NbtCompound) {
+                            NbtCompound compound = (NbtCompound) base;
+                            int x = compound.getInteger("x");
+                            int y = compound.getInteger("y");
+                            int z = compound.getInteger("z");
+                            Material material = new Location(event.getPlayer().getWorld(), x, y, z).getBlock().getType();
+                            if (blocks.contains(material.name())) {
+                                iterator.remove();
+                            }
+                        }
+                    }
+                    event.getPacket().getListNbtModifier().write(0, list);
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            reloadConfig();
+            sender.sendMessage("reload complete!");
+            return true;
+        }
+
+        Player player = (Player) sender;
+        UUID uuid = player.getUniqueId();
+
+        if (args.length == 0) {
+            sender.sendMessage(toggle(uuid) ? "ON" : "OFF");
+        } else {
+            switch (args[0].toLowerCase()) {
+                case "on": {
+                    players.add(uuid);
+                    sender.sendMessage("ON");
+                    break;
+                }
+                case "off": {
+                    players.remove(uuid);
+                    sender.sendMessage("OFF");
+                    break;
+                }
+                case "reload": {
+                    if (player.hasPermission("notileentity.admin")) {
+                        reloadConfig();
+                        blocks = getConfig().getStringList("blocks");
+                        sender.sendMessage("reload complete!");
+                        break;
+                    }
+                }
+                default: {
+                    sender.sendMessage(toggle(uuid) ? "ON" : "OFF");
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean toggle(UUID uuid) {
+        if (players.contains(uuid)) {
+            players.remove(uuid);
+            return false;
+        } else {
+            players.add(uuid);
+            return true;
+        }
+    }
 }
